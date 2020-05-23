2c9ebdb9bf40ffc4fac94a4740619cd768b0941d
Michael Fernandez <mikevfern@gmail.com>
2020-05-23 12:19:59 -0700

    refactor. setup post /index endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const express = require('express')
-const app = express()
+require('./db/mongoose')
+const User = require('./models/user')
 
+
+const app = express()
 const port = process.env.PORT || 3000
+
+app.use(express.json()) // Parse incoming json to obj.
+
+app.post('/index', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.send(user)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
 
 app.listen(port, () => {
     console.log("Server is on port " + port)
