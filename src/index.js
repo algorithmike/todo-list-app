ce147309c1de52bb196719101cc1299a6ed6f8fe
Michael Fernandez <mikevfern@gmail.com>
2020-05-25 11:59:03 -0700

    prog

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ const port = process.env.PORT || 3000
 
 app.use(express.json()) // Parse incoming json to obj.
 
+//
+// CREATE
+//
 app.post('/index', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((error) => {
         res.status(400).send(error)
     })
@@ -23,11 +26,55 @@ app.post('/task', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
     })
 })
+
+//
+// READ
+//
+app.get('/users', (req, res) => {
+    User.find({}).then((result) => {
+        res.send(result)
+    }).catch(() => {
+        res.status(500).send()
+    })
+})
+
+app.get('/user/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((result) => {
+        if(!result) return res.status(404).send()
+
+        res.send(result)
+    }).catch((() => {
+        res.status(500).send()
+    }))
+})
+
+app.get('/tasks',(req, res) => {
+    Task.find({}).then((result) => {
+        res.send(result)
+    }).catch(() => {
+        res.status(500).send()
+    })
+})
+
+app.get('/task/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((result) => {
+        if(!result) return res.status(404).send()
+
+        res.send(result)
+    }).catch(() => {
+        res.status(500).send()
+    })
+})
+
 
 app.listen(port, () => {
     console.log("Server is on port " + port)
