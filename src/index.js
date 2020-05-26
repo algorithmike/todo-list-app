b3f31135635127aaa7028f7d5dc63192619093a1
Michael Fernandez <mikevfern@gmail.com>
2020-05-26 13:04:44 -0700

    'delete' endpoints

--- src/index.js
+++ src/index.js
@@ -106,6 +106,53 @@ app.patch('/user/:id', async (req, res) => {
     }
 })
 
+app.patch('/task/:id', async (req, res) => {
+    const _id = req.params.id
+    const args = req.body
+    const allowedUpdates = ['completed', 'description']
+    const isValid = Object.keys(args).every((arg) => allowedUpdates.includes(arg))
+
+    if(!isValid) return res.status(400).send({error: "Invalid update."})
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, args, { new: true, runValidators: true })
+
+        if(!task) return res.status(404).send()
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//
+// DELETE
+//
+app.delete('/user/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findByIdAndDelete(_id)
+
+        if(!user) return res.status(404).send()
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/task/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+
+        if(!task) return res.status(404).send()
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.listen(port, () => {
     console.log("Server is on port " + port)
 })
