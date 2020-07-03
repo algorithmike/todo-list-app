28d83bc0e90bc5629d6f722332418b91a6e2f9de
Michael Fernandez <mikevfern@gmail.com>
2020-07-02 18:20:56 -0700

    send test email

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,2 +1,13 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const msg = {
+    to: process.env.TO_AND_FROM_EMAIL,
+    from: process.env.TO_AND_FROM_EMAIL,
+    subject: 'Test subject',
+    text: 'Test email text',
+    html: '<strong>Test html</strong>',
+  };
+  sgMail.send(msg).catch(x => {
+      console.log(x)
+  });
