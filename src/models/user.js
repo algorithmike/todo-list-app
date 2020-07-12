13c7bf54c1230830d8f29ceb3e63512a4901fbcd
Michael Fernandez <mikevfern@gmail.com>
2020-07-12 14:35:33 -0700

    refactor

--- src/models/user.js
+++ src/models/user.js
@@ -69,7 +69,7 @@ userSchema.methods.toJSON = function() {
 }
 
 userSchema.methods.generateAuthToken = async function() {
-    const token =  await jwt.sign({ _id:  this._id.toString()}, 'placeholderPrivateKey')
+    const token =  await jwt.sign({ _id:  this._id.toString()}, process.env.JWT_SECRET)
 
     this.tokens = this.tokens.concat({token})
     await this.save()
