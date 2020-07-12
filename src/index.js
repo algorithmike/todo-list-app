e03ddee200a06b02ed216dc22ef7176505e8a8e6
Michael Fernandez <mikevfern@gmail.com>
2020-07-12 14:16:33 -0700

    refactor

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const taskRouter = require('../src/routers/task')
 
 
 const app = express()
-const port = process.env.PORT || 3000
-
+const port = process.env.PORT
 app.use(express.json()) // Parse incoming json to obj.
 app.use(userRouter)
 app.use(taskRouter)
